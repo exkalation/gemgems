79b671bc8618c6ab28dc57fd294e568f3416ae03
Thomas Karl <thomas@xciteit.com>
2017-09-19 23:42:18 +0200

    implemented elimination!!!

--- src/RenderSvg.elm
+++ src/RenderSvg.elm
@@ -1,54 +1,45 @@
 module RenderSvg exposing (..)
 
+import Json.Decode as Json exposing (succeed)
 import Svg exposing (..)
 import Svg.Attributes as SvgA exposing (..)
 import Svg.Events as SvgE exposing (..)
 
-base : (Float, Float) -> String
-base (dx, dy) =
+
+base : String
+base =
     [ (30.1,84.5), (10.2,50), (30.1,15.5), (69.9,15.5), (89.8,50), (69.9,84.5) ]
-        |> List.map (\(a, b) -> (toString (a + dx)) ++ "," ++ (toString (b + dy)))
+    --[ (32.500,0.000), (16.250,28.146), (-16.250,28.146), (-32.500,0.000), (-16.250,-28.146), (16.250,-28.146) ]
+        --|> List.map (\(a, b) -> (toString (0.95*a + dx)) ++ "," ++ (toString (0.95*b + dy)))
+        |> List.map (\(a, b) -> (toString a) ++ "," ++ (toString b))
+        --|> String.join "," --<<-- try!!
         |> List.foldl (\a b -> if b /= "" then a ++ " " ++ b else a) ""
 
-rectGem (b, a) props =
-    let
-        p = base (a, b)
-    in
-    --rect (List.append [ SvgA.x (toString (a*scale)), SvgA.y (toString (b*scale)), SvgA.width "30", SvgA.height "30", SvgA.rx "5", SvgA.ry "5" ] props) []
-    --Svg.path List.append props [ SvgA.transform "scale(0.4)", fill "#ff0", SvgA.x (toString a), SvgA.y (toString b), d "M2.5000000000000004 47.63139720814412Q0 43.30127018922193 2.5000000000000004 38.97114317029974L22.5 4.330127018922193Q25 0 30 0L70 0Q75 0 77.5 4.330127018922193L97.5 38.97114317029974Q100 43.30127018922193 97.5 47.63139720814412L77.5 82.27241335952166Q75 86.60254037844386 70 86.60254037844386L30 86.60254037844386Q25 86.60254037844386 22.5 82.27241335952166Z" ] []
-    polygon (List.append props [ transform "scale(1)", SvgA.x (toString a), SvgA.y (toString b), points p ]) []
-
-redSquare (a, b) msgDown msgUp =
-    rectGem (a, b) [ fill "#f00", onMouseDown msgDown, onMouseUp msgUp ]
-blueSquare (a, b) msgDown msgUp =
-    rectGem (a, b) [ fill "#0B79CE", onMouseDown msgDown, onMouseUp msgUp ]
-blackSquare (a, b) msgDown msgUp =
-    rectGem (a, b) [ fill "#000", onMouseDown msgDown, onMouseUp msgUp ]
-greenSquare (a, b) msgDown msgUp =
-    rectGem (a, b) [ fill "#0f0", onMouseDown msgDown, onMouseUp msgUp ]
-highlightSquare (a, b) msgDown msgUp =
-    rectGem (a, b) [ fill "#fff", onMouseDown msgDown, onMouseUp msgUp ]
+onMouseLeave msg =
+    on "mouseLeave" (Json.succeed msg)
 
 drawContainer =
     svg [ width "600", height "600", viewBox "0 0 600 600" ]
 
-drawShape el ab msgDown msgUp =
-    case el of
-        Just 1 -> redSquare ab msgDown msgUp
-        Just 2 -> blueSquare ab msgDown msgUp
-        Just 3 -> greenSquare ab msgDown msgUp
-        Just 4 -> blackSquare ab msgDown msgUp --redCircle ab
-        --Just 5 -> blueCircle ab msgDown msgUp
-        --Just 6 -> greenCircle ab msgDown msgUp
-        Just 9 -> highlightSquare ab msgDown msgUp
-        Nothing -> redSquare ab msgDown msgUp
+shapeTypeToColor gemType =
+    case gemType of
+        Just 1 -> "#9B111E" -- ruby red #9B111E / red #f00
+        Just 2 -> "#0c0" -- emerald #50C878 / green #0c0
+        Just 3 -> "#0F52BA" -- sapphire #0F52BA / blue #07a
+        Just 4 -> "#ff0" -- yellow
+        Just 5 -> "#f30" -- orange
+        Just 6 -> "#60c" -- violet
+        Just 9 -> "#fff" -- dragged
+        Just 10 -> "#aaa" -- eliminated/empty
+        Nothing -> "#000"
         default -> Debug.crash "Unhandled shape tpye!"
 
---circleGem (b, a) props =
---    circle (List.append [ SvgA.cx (toString (a+15)), SvgA.cy (toString (b+15)), SvgA.r "15" ] props) []
---redCircle (a, b) =
---    circleGem (a, b) [ fill "#f00" ]
---blueCircle (a, b) =
---    circleGem (a, b) [ fill "#0B79CE" ]
---greenCircle (a, b) =
---    circleGem (a, b) [ fill "#0f0" ]
+drawShape gemType (x, y) msgDown msgUp =
+    let
+        (dx, dy) = (y, x)
+        color = shapeTypeToColor gemType
+    in
+        g [ transform ("translate(" ++ (toString dx) ++ "," ++ (toString dy) ++ ") scale(0.91)") ]
+        [ polygon [ fill color, SvgA.stroke "#000", onMouseDown msgDown, onMouseUp msgUp, points base ] []
+        ]
+
--- src/main.elm
+++ src/main.elm
@@ -28,7 +28,7 @@ type alias Model =
     { gems : Dict.Dict HexMap.Hash Int
     , grid: HexMap.Map
     , layout: HexL.Layout
-    , dragged: Maybe (HexMap.Hash, Int)
+    , dragged: Maybe (HexMap.Hash, Hex, Int)
     }
 
 init : (Model, Cmd Msg)
@@ -39,7 +39,7 @@ init =
         (Model
             Dict.empty
             grid
-            (HexL.Layout (orientationLayoutPointy) (39, 39) (20, 20))
+            (HexL.Layout (orientationLayoutPointy) (38, 38) (0, 0))
             Nothing
         , rollAll grid)
 
@@ -73,50 +73,128 @@ update msg model =
 
             DragStart (k, v) ->
                 let
-                    currentValue = case (Dict.get k model.gems) of
+                    dbg = Debug.log "DRAG_START" v
+                    gemType = case (Dict.get k model.gems) of
                         Just x -> x
-                        Nothing -> Debug.crash "Not possible..."
-                    unhighlightedGems = unhighlightDraged model.dragged model.gems
+                        Nothing -> Debug.crash "Not possible: DragStart"
+                    m1 = (unhighlight model)
                 in
-                    ({ model | gems = highlightDraged k unhighlightedGems, dragged = Just (k, currentValue) }, Cmd.none)
+                    ({ m1 | gems = highlightDraged k m1.gems, dragged = Just (k, v, gemType) }, Cmd.none)
 
             DragEnd (k, v) ->
                 let
-                    dbg = Debug.log "CLICK" (k, v)
-                    ne = neighbor v NE
-                    e = neighbor v E
-                    dbg2 = Debug.log "NEIGHBORS" [ (v, HexMap.hashHex v), (ne, HexMap.hashHex ne), (e, HexMap.hashHex e) ]
+                    dbg = Debug.log "DRAG_END" v
+                    --ne = neighbor v NE
+                    --e = neighbor v E
+                    --dbg2 = Debug.log "NEIGHBORS" [ (v, HexMap.hashHex v), (ne, HexMap.hashHex ne), (e, HexMap.hashHex e) ]
+                    targetGemType = case (Dict.get k model.gems) of
+                        Just x -> x
+                        Nothing -> 0
+                    (draggedHash, draggedHex, draggedGemType) = getDragged model
+                    m1 =
+                        if (Hex.distance draggedHex v) == 1 && (draggedGemType /= targetGemType) then
+                            swapGems (k, v) model
+                                |> eliminateFrom (k, v)
+                                |> eliminateFrom (draggedHash, draggedHex)
+                        else unhighlight model
                 in
-                    ({ model | gems = unhighlightDraged model.dragged model.gems, dragged = Nothing }, Cmd.none)
+                    ({ m1 | dragged = Nothing }, Cmd.none)
+
+runElimination model =
+    Dict.toList model.grid
+        |> List.map ((flip eliminateFrom) model)
+
+eliminateFrom (hash, hex) model =
+    let
+        gemType = Debug.log "ELIMINATEFROM" (getGemType hash model.gems)
+    in
+        scan model gemType hex []
+            |> eliminateFound model
+
+eliminateFound model found =
+    let
+        x = Debug.log "TO BE ELIMIATED" found
+    in
+        if (List.length found) > 2 then { model | gems = model.grid
+            |> Dict.map (\hash hex -> if List.member hex found then 10 else (getGemType hash model.gems))
+            --|> Dict.fromList
+        }
+        else model
+
+scan model scanGemType fromHex found =
+    let
+        x = Debug.log "SCAN" (fromHex, found)
+        visited = List.member fromHex found
+        fromHash = HexMap.hashHex fromHex
+        candidate = (not visited) && case (Dict.get fromHash model.gems) of
+            Just x -> x == scanGemType
+            Nothing -> False -- we reached the border...
+        f = if candidate then fromHex :: found else found
+    in
+        --if List.filter found  |> List.length > 0
+        if candidate then f
+            |> scan model scanGemType (neighbor fromHex NE)
+            |> scan model scanGemType (neighbor fromHex E)
+            |> scan model scanGemType (neighbor fromHex SE)
+            |> scan model scanGemType (neighbor fromHex SW)
+            |> scan model scanGemType (neighbor fromHex W)
+            |> scan model scanGemType (neighbor fromHex NW)
+        else f
+
+swapGems (hashTarget, _) model =
+    let
+        (hashDragged, _, gemTypeDragged) = getDragged model
+        gemTypeTarget = getGemType hashTarget model.gems
+        gems1 = model.gems
+            |> Dict.insert hashDragged gemTypeTarget
+            |> Dict.insert hashTarget gemTypeDragged
+    in
+        Debug.log "MODEL" { model | gems = gems1 }
+
+getDragged model =
+    case model.dragged of
+        Just dragged -> dragged
+        Nothing -> Debug.crash "Not possible: getDragged"
+
+--getHex hash model =
+--    case model.grid of
+--        Just hex -> hex
+--        Nothing -> Debug.crash "Not possible: getHex"
+
+getGemType hash gems =
+    case (Dict.get hash gems) of
+       Just gemType -> gemType
+       Nothing -> Debug.crash ("Not possible: getGemType " ++ (toString hash) ++ (toString gems))
+
+unhighlight model =
+    { model | gems = case model.dragged of
+         Just (hash, hex, gemType) -> Dict.insert hash gemType model.gems
+         Nothing -> model.gems, dragged = Nothing
+     }
 
 rollAll hexMap =
     hexMap
         |> Dict.keys
-        |> List.map (\x -> Random.generate (NewFace x) (Random.int 1 4))
+        |> List.map (\x -> Random.generate (NewFace x) (Random.int 1 6))
         |> Cmd.batch
 
-highlightDraged dragged gems =
-    Dict.insert dragged 9 gems
+highlightDraged draggedHash gems =
+    Dict.insert draggedHash 9 gems
 
-unhighlightDraged : Maybe (HexMap.Hash, Int) -> Dict.Dict HexMap.Hash Int -> Dict.Dict HexMap.Hash Int
-unhighlightDraged dragged gems =
-    case dragged of
-        Just (hash, gemType) -> Dict.insert hash gemType gems
-        Nothing -> gems
 
 -- VIEW
 
 view : Model -> Html Msg
 view model =
     let
-        dbg = Debug.log "VIEW - model.gems" model.gems
+        dbg = model --Debug.log "VIEW - model.gems" model.gems
     in
         div []
             [ div [ id "board" ]
                 [ model.grid
-                    |> Dict.map (\k v -> Render.drawShape (Dict.get k model.gems) (HexL.hexToPoint model.layout v) (DragStart (k, v)) (DragEnd (k, v)))
+                    |> Dict.map (\k v -> drawShape (Dict.get k model.gems) (HexL.hexToPoint model.layout v) (DragStart (k, v)) (DragEnd (k, v)))
                     |> Dict.values
-                    |> Render.drawContainer
+                    |> drawContainer
                 ]
             , button [ onClick Roll ] [ Html.text "Roll" ]
             ]
