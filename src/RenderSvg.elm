e74fc72cf85b531eb61f64f7cac576b1397e2520
Thomas Karl <thomas@xciteit.com>
2017-09-21 23:38:35 +0200

    simplifcation of joining hex points list

--- src/RenderSvg.elm
+++ src/RenderSvg.elm
@@ -10,10 +10,8 @@ import Gem exposing (..)
 base : String
 base =
     [ (30.1,84.5), (10.2,50), (30.1,15.5), (69.9,15.5), (89.8,50), (69.9,84.5) ]
-    --[ (32.500,0.000), (16.250,28.146), (-16.250,28.146), (-32.500,0.000), (-16.250,-28.146), (16.250,-28.146) ]
-        --|> List.map (\(a, b) -> (toString (0.95*a + dx)) ++ "," ++ (toString (0.95*b + dy)))
-        |> List.map (\(a, b) -> (toString a) ++ "," ++ (toString b))
-        |> String.join ","
+    |> List.map (\(a, b) -> (toString a) ++ "," ++ (toString b))
+    |> String.join ", "
 
 onMouseLeave msg =
     on "mouseLeave" (Json.succeed msg)
@@ -21,8 +19,8 @@ onMouseLeave msg =
 drawContainer =
     svg [ width "600", height "600", viewBox "0 0 600 600" ]
 
-shapeTypeToColor gemType =
-    case gemType of
+gemToColor gem =
+    case gem of
         Just (Gem.Color 1) -> "#9B111E" -- ruby red #9B111E / red #f00
         Just (Gem.Color 2) -> "#0c0" -- emerald #50C878 / green #0c0
         Just (Gem.Color 3) -> "#0F52BA" -- sapphire #0F52BA / blue #07a
@@ -39,7 +37,7 @@ shapeTypeToColor gemType =
 drawShape gemType (x, y) msgDown msgUp =
     let
         (dx, dy) = (y, x)
-        color = shapeTypeToColor gemType
+        color = gemToColor gemType
     in
         g [ transform ("translate(" ++ (toString dx) ++ "," ++ (toString dy) ++ ") scale(0.91)") ]
         [ polygon [ fill color, SvgA.stroke "#000", onMouseDown msgDown, onMouseUp msgUp, points base ] []
