ac5a1f752fc08328d7c5c2cc9a7978e26327beb1
Thomas Karl <thomas@xciteit.com>
2017-12-31 21:58:54 +0100

    further cleaning up RenderSvg, getting rid of unused relics

--- src/RenderSvg.elm
+++ src/RenderSvg.elm
@@ -7,7 +7,6 @@ import Gem
 
 base : String
 base =
-    --[ (30.1,84.5), (10.2,50), (30.1,15.5), (69.9,15.5), (89.8,50), (69.9,84.5) ]
     [ (20,70), (0,35), (20,0), (60,0), (80,35), (60,70) ]
     |> List.map (\(a, b) -> (toString a) ++ "," ++ (toString b))
     |> String.join ", "
@@ -17,21 +16,23 @@ drawContainer =
 
 gemToColor gem =
     case gem of
-        Just (Gem.Color c) -> (Gem.toString (Gem.Color c), "color type-" ++ (toString c), [])
-        Just (Gem.Matched c) -> (Gem.toString (Gem.Matched c), "matched type-" ++ (toString c), [])
-        Just Gem.Dragged -> (Gem.toString Gem.Dragged, "type-dragged", [])
-        Just (Gem.Moving c y) -> (Gem.toString (Gem.Moving c y), "type-moving color type-" ++ (toString c), [])
-        Just Gem.Empty -> (Gem.toString Gem.Empty, "type-empty", [])
-        Nothing -> ("Nothing", "type-empty", [])
+        Just (Gem.Color c) -> "color type-" ++ (toString c)
+        Just (Gem.Matched c) -> "matched type-" ++ (toString c)
+        Just (Gem.Moving c y) -> "type-moving color type-" ++ (toString c)
+        Just Gem.Dragged -> "type-dragged"
+        Just Gem.Empty -> "type-empty"
+        Nothing -> "type-empty"
 
 drawShape gemType (x, y) msgDown msgUp =
     let
-        (zz, color, animations) = gemToColor gemType
+        gs = Maybe.withDefault Gem.Empty gemType |> Gem.toString
+        color = gemToColor gemType
         yy = case gemType of
             Just (Gem.Moving _ dist) -> y - dist
             default -> y
+        (xs, ys) = (toString x, toString yy)
+        gids = "id-g-" ++ gs ++ "-" ++ xs ++ "-" ++ ys
+        pids = gids ++ color
     in
-        g [ id ("id-g-" ++ zz ++ "-" ++ (toString x) ++ "-" ++ (toString yy)), transform ("translate(" ++ (toString x) ++ "," ++ (toString yy) ++ ")") ]
-            [ polygon [ id ("id-p-" ++ zz ++ "-" ++ (toString x) ++ "-" ++ (toString yy) ++ "-" ++ color), class color, SvgA.stroke "#000", onMouseDown msgDown, onMouseUp msgUp, points base ]
-                animations
-            ]
+        g [ id gids, transform ("translate(" ++ xs ++ "," ++ ys ++ ")") ]
+            [ polygon [ id pids, class color, SvgA.stroke "#000", onMouseDown msgDown, onMouseUp msgUp, points base ] []]
