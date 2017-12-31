07add48457cb5b8fa2e87be6a45bd338b2fdca5c
Thomas Karl <thomas@xciteit.com>
2017-12-31 21:31:32 +0100

    cleaning up RenderSvg, using more readable coordinates for hexagon SVG, setting base SVG sizes more appropriately and cleaning up unused and badly named arguments

--- src/RenderSvg.elm
+++ src/RenderSvg.elm
@@ -5,17 +5,17 @@ import Svg.Attributes as SvgA exposing (..)
 import Svg.Events as SvgE exposing (..)
 import Gem
 
-
 base : String
 base =
-    [ (30.1,84.5), (10.2,50), (30.1,15.5), (69.9,15.5), (89.8,50), (69.9,84.5) ]
+    --[ (30.1,84.5), (10.2,50), (30.1,15.5), (69.9,15.5), (89.8,50), (69.9,84.5) ]
+    [ (20,70), (0,35), (20,0), (60,0), (80,35), (60,70) ]
     |> List.map (\(a, b) -> (toString a) ++ "," ++ (toString b))
     |> String.join ", "
 
 drawContainer =
-    svg [ width "420", height "560", viewBox "0 0 420 560", stroke "#f00" ]
+    svg [ width "100%", viewBox "0 0 410 540", stroke "#f00" ]
 
-gemToColor gem xs ys =
+gemToColor gem =
     case gem of
         Just (Gem.Color c) -> (Gem.toString (Gem.Color c), "color type-" ++ (toString c), [])
         Just (Gem.Matched c) -> (Gem.toString (Gem.Matched c), "matched type-" ++ (toString c), [])
@@ -24,25 +24,14 @@ gemToColor gem xs ys =
         Just Gem.Empty -> (Gem.toString Gem.Empty, "type-empty", [])
         Nothing -> ("Nothing", "type-empty", [])
 
-drawShape gemType (x, y2) msgDown msgUp =
+drawShape gemType (x, y) msgDown msgUp =
     let
-        (zz, color, animations) = gemToColor gemType (toString 50) (toString 50)
-        y = case gemType of
-            Just (Gem.Moving _ dist) -> y2 - dist
-            default -> y2
+        (zz, color, animations) = gemToColor gemType
+        yy = case gemType of
+            Just (Gem.Moving _ dist) -> y - dist
+            default -> y
     in
-        g [ id ("id-g-" ++ zz ++ "-" ++ (toString x) ++ "-" ++ (toString y)), transform ("translate(" ++ (toString x) ++ "," ++ (toString y) ++ ")") ]
-            [ polygon [ id ("id-p-" ++ zz ++ "-" ++ (toString x) ++ "-" ++ (toString y) ++ "-" ++ color), class color, SvgA.stroke "#000", onMouseDown msgDown, onMouseUp msgUp, points base ]
+        g [ id ("id-g-" ++ zz ++ "-" ++ (toString x) ++ "-" ++ (toString yy)), transform ("translate(" ++ (toString x) ++ "," ++ (toString yy) ++ ")") ]
+            [ polygon [ id ("id-p-" ++ zz ++ "-" ++ (toString x) ++ "-" ++ (toString yy) ++ "-" ++ color), class color, SvgA.stroke "#000", onMouseDown msgDown, onMouseUp msgUp, points base ]
                 animations
             ]
-
-{-
-animateTransform
-                               [ attributeName "transform"
-                               , SvgA.type_ "translate"
-                               , from ((toString x) ++ " 0")
-                               , to ((toString x) ++ " " ++ (toString y))
-                               , dur "0.2s", repeatCount "1"
-                               ] []
-            ,
--}
